ee30caeb25ed591d7b14f4979ebb2e1437c44c43
Patrick van Loon <p.j.vanloon1991@gmail.com>
2019-06-19 16:26:05 +0200

    Remove step collection constructor

--- src/Startable.php
+++ src/Startable.php
@@ -6,6 +6,5 @@ interface Startable
 {
     public function isStarted(): bool;
 
-    // TODO revise this data here.. maybe use setData for the flow..?
     public function start(object $data): void;
 }
