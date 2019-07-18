a07614d0c505052e3ceb33184e950c2fb229f75c
Patrick van Loon <p.j.vanloon1991@gmail.com>
2019-07-18 19:30:51 +0200

    Added registry setup.

---
+++ src/Registry.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types = 1);
+
+namespace Aeviiq\FormFlow;
+
+use Aeviiq\Factory\AbstractServiceFactory;
+
+final class Registry extends AbstractServiceFactory implements RegistryInterface
+{
+    // TODO think if this service factory is indeed the best choice for this purpose.
+    protected function getTargetInterface(): string
+    {
+        return FormFlowInterface::class;
+    }
+}
