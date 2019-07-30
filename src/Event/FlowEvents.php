e60bf5cdf1833251f38c1d1a897931fdc7dba505
Patrick van Loon <p.j.vanloon1991@gmail.com>
2019-07-30 19:08:43 +0200

    Adjusted the event values.

--- src/Event/FlowEvents.php
+++ src/Event/FlowEvents.php
@@ -4,12 +4,12 @@ namespace Aeviiq\FormFlow\Event;
 
 final class FlowEvents
 {
-    public const PRE_START = 'aeviiq.flow.event.pre_start';
-    public const POST_START = 'aeviiq.flow.event.post_start';
-    public const PRE_NEXT = 'aeviiq.flow.event.pre_next';
-    public const POST_NEXT = 'aeviiq.flow.event.post_next';
-    public const PRE_PREVIOUS = 'aeviiq.flow.event.pre_previous';
-    public const POST_PREVIOUS = 'aeviiq.flow.event.post_previous';
-    public const PRE_COMPLETE = 'aeviiq.flow.event.pre_complete';
-    public const POST_COMPLETE = 'aeviiq.flow.event.post_complete';
+    public const PRE_START = 'aeviiq.form_flow.event.pre_start';
+    public const POST_START = 'aeviiq.form_flow.event.post_start';
+    public const PRE_NEXT = 'aeviiq.form_flow.event.pre_next';
+    public const POST_NEXT = 'aeviiq.form_flow.event.post_next';
+    public const PRE_PREVIOUS = 'aeviiq.form_flow.event.pre_previous';
+    public const POST_PREVIOUS = 'aeviiq.form_flow.event.post_previous';
+    public const PRE_COMPLETE = 'aeviiq.form_flow.event.pre_complete';
+    public const POST_COMPLETE = 'aeviiq.form_flow.event.post_complete';
 }
