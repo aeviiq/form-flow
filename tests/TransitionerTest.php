43666e654ed223fca4a43be46891dd3102a7d93a
Patrick van Loon <p.j.vanloon1991@gmail.com>
2019-08-15 19:03:25 +0200

    Added empty unit test cases.

---
+++ tests/TransitionerTest.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace Aeviiq\FormFlow\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class TransitionerTest extends TestCase
+{
+    public function testTransitionStart(): void
+    {
+
+    }
+
+    public function testTransition(): void
+    {
+
+    }
+
+    public function testTransitionForwards(): void
+    {
+
+    }
+
+    public function testTransitionForwardsFiresEvents(): void
+    {
+
+    }
+
+    public function testTransitionForwardsCanBeBlocked(): void
+    {
+
+    }
+
+    public function testTransitionBackwards(): void
+    {
+
+    }
+
+    public function testTransitionBackwardsFiresEvents(): void
+    {
+
+    }
+
+    public function testTransitionBackwardsCanBeBlocked(): void
+    {
+
+    }
+
+    public function testTransitionComplete(): void
+    {
+
+    }
+
+    public function testTransitionReset(): void
+    {
+
+    }
+
+    public function testWithoutRequestStack(): void
+    {
+
+    }
+}
