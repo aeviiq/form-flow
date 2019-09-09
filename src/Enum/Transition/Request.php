873961f965b0612715de7855349526d73797d060
Patrick van Loon <p.j.vanloon1991@gmail.com>
2019-09-09 19:29:20 +0200

    Created static construct method for the transition request.

--- src/Enum/Transition/Request.php
+++ src/Enum/Transition/Request.php
@@ -4,6 +4,8 @@ namespace Aeviiq\FormFlow\Enum\Transition;
 
 use Aeviiq\Enum\AbstractEnum;
 use Aeviiq\FormFlow\Exception\InvalidArgumentException;
+use Aeviiq\FormFlow\FormFlowInterface;
+use Symfony\Component\HttpFoundation\Request as HttpRequest;
 
 final class Request extends AbstractEnum
 {
@@ -20,7 +22,7 @@ final class Request extends AbstractEnum
      */
     private $requestedStepNumber;
 
-    public function __construct(string $value, int $requestedStepNumber = 0)
+    private function __construct(string $value, int $requestedStepNumber)
     {
         parent::__construct($value);
         if ($requestedStepNumber < 0) {
@@ -36,6 +38,30 @@ final class Request extends AbstractEnum
         }
 
         $this->requestedStepNumber = $requestedStepNumber;
+    }
+
+    public static function createByHttpRequestAndFlow(HttpRequest $httpRequest, FormFlowInterface $flow): self
+    {
+        $transition = (string)$httpRequest->get($flow->getTransitionKey(), '');
+        if (self::isValid($transition)) {
+            if (self::FORWARDS === $transition) {
+                $stepNumber = $flow->getCurrentStepNumber() + 1;
+            } elseif (self::BACKWARDS === $transition) {
+                $stepNumber = $flow->getCurrentStepNumber() - 1;
+            }
+
+            return new self($transition, $stepNumber ?? 0);
+        }
+
+        $transitions = \explode('_', $transition);
+        if (isset($transitions[0], $transitions[1])) {
+            [$action, $stepNumber] = $transitions;
+            if (self::isValid($action) && \is_numeric($stepNumber) && $stepNumber > 0 && $stepNumber <= $flow->getSteps()->count()) {
+                return new self($action, (int)$stepNumber);
+            }
+        }
+
+        throw new InvalidArgumentException(\sprintf('"%s" is an invalid transition request for flow "%s".', $transition, $flow->getName()));
     }
 
     public function getRequestedStepNumber(): int
